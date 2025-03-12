6b0adc4b783ec777e85c94ad0aaf7958327401e9
Your Name <your_email@example.com>
2025-03-12 12:27:49 -0700

    layout

--- src/app/layout.js
+++ src/app/layout.js
@@ -5,6 +5,7 @@ import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css"; // Import Toastify styles
 import { Toaster } from "react-hot-toast";
 import { ContextProvider } from "./Context/ContextPage";
+import Script from "next/script"; // Import Next.js Script
 
 // Define fonts with localFont
 const geistSans = localFont({
@@ -30,9 +31,35 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en" data-color-mode="dark">
+      <head>
+        {/* Google Tag Manager - Head */}
+        <Script
+          id="gtm-head"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-P3HW53NP');
+            `,
+          }}
+        />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        {/* Google Tag Manager - Body */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-P3HW53NP"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
+
         <ContextProvider>
           <NextTopLoader /> {/* Top loader for navigation transitions */}
           <Toaster /> {/* Hot Toast notifications */}
