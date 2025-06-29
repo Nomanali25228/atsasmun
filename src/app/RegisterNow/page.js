80dd57e914f2ae5f1a1149c3b437fbd110cd8a63
Noman <156717218+Nomanali25228@users.noreply.github.com>
2025-06-29 13:46:29 -0700

    Update page.js

--- src/app/RegisterNow/page.js
+++ src/app/RegisterNow/page.js
@@ -890,8 +890,8 @@ setLoader(false)
                       {destination === "" ? "" : "at"} {check === "" ? destination : check}
                     </p>
                     <p className="text-sm sm:text-base md:text-lg mt-1">
-                      {destination === "" ? "" : "from "} {change.from}{destination === "" ? "" : " to "} {change.to}
-                      <sup>{destination === "" ? "" : "th "}</sup> {change.th}
+                     {destination === "" ? "" : "from "} {change.from}   {destination === "" ? "" : "to "} {change.to}{} {change.th}
+
                     </p>
                     </div>
                   </div>
